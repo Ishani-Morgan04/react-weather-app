21c6f7c89a3977f777601a9d11955d5c11402e5c
Ishani-Morgan04 <ishani.morgan@gmail.com>
2022-09-23 01:00:33 +0800

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Ishani Morgan and it is {""}
+        <a
+          href="https://github.com/Ishani-Morgan04/react-weather-app"
+          target="_blank"
+        >
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
