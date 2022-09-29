c04484f850f42ea29416b69c824decf8800efacd
Ishani-Morgan04 <ishani.morgan@gmail.com>
2022-09-30 00:03:59 +0800

    Updated FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -30,13 +30,13 @@ export default function FormattedDate(props) {
   let date = props.date.getDate();
   let month = months[props.date.getMonth()];
   let year = props.date.getFullYear();
+
   let hours = props.date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
 
   let minutes = props.date.getMinutes();
-
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
